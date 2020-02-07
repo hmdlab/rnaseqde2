6dfe49abadb8a8fb75fe8867f46b77994b659c93
YH <yu.hamaguchi@hamadalab.com>
2020-02-07 13:17:49 +0900

    Change target to all assets.yml

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,30 +3,36 @@ This is test for rnaseqde.utils
 """
 
 import unittest
+from glob import glob
 
 import rnaseqde.utils as utils
 
 
 class TestConfig(unittest.TestCase):
     def test_load_conf(self, relpath='config/assets.yml'):
-        conf = utils.load_conf(relpath)
 
-        # NOTE: Structure: genome-annotation-tool-path(value)
-        paths = []
-        for a in conf.values():
-            for t in a.values():
-                for p in t.values():
-                    paths.append(p)
+        configs = glob('config/assets*.yml')
 
-        existences = list(map(utils.exists, paths))
+        for c in configs:
+            print(f"Checking {c}...")
+            conf = utils.load_conf(c)
 
-        for p, e in zip(paths, existences):
-            print(
-                "{}: {}".format(p,
-                                e
-                                ))
+            # NOTE: Structure: genome-annotation-tool-path(value)
+            paths = []
+            for a in conf.values():
+                for t in a.values():
+                    for p in t.values():
+                        paths.append(p)
 
-        self.assertTrue(all(existences))
+            existences = list(map(utils.exists, paths))
+
+            for p, e in zip(paths, existences):
+                print(
+                    "{}: {}".format(p,
+                                    e
+                                    ))
+
+            self.assertTrue(all(existences))
 
 
 if __name__ == '__main__':
